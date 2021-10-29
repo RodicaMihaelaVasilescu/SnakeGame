0f8a38e0d5b02449ab64cd83231cf67acf8fa884
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2021-10-29 21:13:04 +0300

    add project

---
+++ SnakeGame/SnakeGame/View/SnakeBoardControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnakeGame.View
+{
+  /// <summary>
+  /// Interaction logic for SnakeBoardControl.xaml
+  /// </summary>
+  public partial class SnakeBoardControl : UserControl
+  {
+    public SnakeBoardControl()
+    {
+      InitializeComponent();
+    }
+  }
+}
